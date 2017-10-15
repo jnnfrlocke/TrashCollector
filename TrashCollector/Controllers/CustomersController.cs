f92ac541f70db3dfaa9345039a1395d2f15a7949
Jen <jnnfrlocke@gmail.com>
2017-10-14 23:49:29 -0500

    Remove billing infor from edit customer controller

--- TrashCollector/Controllers/CustomersController.cs
+++ TrashCollector/Controllers/CustomersController.cs
@@ -77,7 +77,7 @@ namespace TrashCollector.Models
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,StreetNumber,StreetName,City,ZipCode,PhoneNumber,CollectionDay,CollectionDates,CostPerPickup,Balance,CreditCard")] Customer customer)
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,StreetNumber,StreetName,City,ZipCode,PhoneNumber,CollectionDay,CreditCard")] Customer customer)
         {
             if (ModelState.IsValid)
             {
